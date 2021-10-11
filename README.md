d325e93a5203d8d1acca72142e598b2d99694823
David Tierney <daveshouse44@hotmail.com>
2021-10-11 13:54:46 -0500

    added name to README, bid routes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gp3_proj2
+# DevNonDev
 
 Our initial project proposal is for a gig work/gig worker connection site.
 Our site will provide a niche in the gig worker market that is specifically geared towards connecting dev and non devs for any given technology related task.
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 // const nondevRoutes = require("./nondev-routes");
 const jobRoutes = require("./job-routes");
+const bidRoutes = require("./bid-routes");
 // const ratingRoutes = require("./rating-routes");
 router.use("/users", userRoutes);
 // router.use("/nondevs", nondevRoutes);
 router.use("/jobs", jobRoutes);
+router.use("/bids", bidRoutes);
 // router.use("/ratings", ratingRoutes);
 
 module.exports = router;
